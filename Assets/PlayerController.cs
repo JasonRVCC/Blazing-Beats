aa28087a1026d6adaf933bda03bb35f21405caaa
JasonRVCC <marcmanzx@yahoo.com>
2017-11-01 21:35:16 -0400

    blah

--- Assets/PlayerController.cs
+++ Assets/PlayerController.cs
@@ -1,18 +1,16 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.UI;
 
 public class PlayerController : MonoBehaviour {
 	
 	[Header("Movement")]
 	public float acceleration;
-	public float maxSpeed;
+	public float maxVelocity;
 	public float deceleration;
 	[Space(10)]
 
 	public int playerNum;
-	public Text collectText;
 
 	[Header("Tilting")]
 	public float leanHorizontal;
@@ -23,55 +21,21 @@ public class PlayerController : MonoBehaviour {
 
 
 	private Transform playerTransform;
+	private Vector3 curVelocity;
 	private Vector3 eulerRotation;
 	private Vector3 curRotation;
-	private int collectCount = 0;
 
 	// Use this for initialization
-	void Start (){
+	void Start () {
 		playerTransform = this.transform;
+		curVelocity = this.gameObject.GetComponent<Rigidbody> ().velocity;
 		curRotation = playerTransform.rotation.eulerAngles;
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		collectText.text = "Orbs" + collectCount;
 		//Player Movement
-		//Vector3 move = playerTransform.rotation * Vector3.forward;
-		/*
-		float axisValue = Input.GetAxis ("Vertical" + playerNum);
-		if (axisValue != 0) {
-			if (axisValue < 0) {
-				curSpeed = Mathf.Max (-maxSpeed, curSpeed - (acceleration * Time.deltaTime));
-			} else {
-				curSpeed = Mathf.Min (maxSpeed, curSpeed + (acceleration * Time.deltaTime));
-			}
-		} else {
-			if (curSpeed > 0) {
-				curSpeed = Mathf.Max (0f, curSpeed - (deceleration * Time.deltaTime));
-			} else if (curSpeed < 0) {
-				curSpeed = Mathf.Min(0f, curSpeed + (deceleration * Time.deltaTime));
-			}
-		}
-
-		//Brake
-		if (Input.GetAxis ("Brake" + playerNum) != 0) {
-			Debug.Log ("BRAKE");
-			if (curSpeed > 0) {
-				curSpeed = Mathf.Max (0f, curSpeed - (deceleration * Time.deltaTime * 2));
-			} else if (curSpeed < 0) {
-				curSpeed = Mathf.Min(0f, curSpeed + (deceleration * Time.deltaTime * 2));
-			}
-		}*/
-
-		//Debug.Log (curSpeed);
-		this.GetComponent<Rigidbody> ().AddForce (transform.forward * acceleration * Input.GetAxis("Vertical"+playerNum), ForceMode.Acceleration);
-		if (this.GetComponent<Rigidbody> ().velocity.magnitude > maxSpeed) {
-			this.GetComponent<Rigidbody> ().velocity = this.GetComponent<Rigidbody> ().velocity.normalized * maxSpeed;
-			Debug.Log (this.GetComponent<Rigidbody> ().velocity.magnitude);
-		}
-		//this.GetComponent<Rigidbody> ().velocity = move * curSpeed;
-		//playerTransform.position += playerTransform.forward * curSpeed/4;
+		playerTransform.position += playerTransform.forward * acceleration * -Input.GetAxis("Vertical"+playerNum) * Time.deltaTime;
 		//this.gameObject.GetComponent<Rigidbody>().AddRelativeForce(new Vector3 (0, 0, acceleration * -Input.GetAxis ("Vertical")));
 		//this.gameObject.GetComponent<Rigidbody> ().velocity = new Vector3 (0, 0, acceleration * -Input.GetAxis ("Vertical"));
 		this.gameObject.transform.Rotate(new Vector3(0,leanHorizontal * Input.GetAxis("Horizontal"+playerNum),0));
@@ -106,13 +70,5 @@ public class PlayerController : MonoBehaviour {
 		playerTransform.Rotate (eulerRotation);
 		curRotation = playerTransform.rotation.eulerAngles;
 		*/
-	}
-
-	void OnTriggerEnter(Collider other){
-		Debug.Log ("hit");
-		if (other.gameObject.tag == "Collectable") {
-			collectCount += 1;
-			GameObject.Destroy (other.gameObject);
-		}
 	}
 }
